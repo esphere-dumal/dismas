8766ffbdd6d521e0fbe4da04a78f0389cd4df700
Esphe <esphere@qq.com>
2023-02-07 23:40:02 -0500

    :sparkles: Better randomSleep

--- prog/randomSleep.go
+++ prog/randomSleep.go
@@ -3,11 +3,22 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
 )
 
 func main() {
-	time.Sleep(7 * time.Second)
+	if len(os.Args) == 2 {
+		timeout, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			fmt.Println("Input is not a number")
+			return
+		}
+		time.Sleep(time.Duration(timeout) * time.Second)
+	} else {
+		time.Sleep(7 * time.Second)
+	}
 	random := rand.Int31()
 	fmt.Println("Random outputs", random)
 }
