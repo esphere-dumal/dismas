ba649445b7845b0dee7928daf5c275c66198c849
Esphe <esphere@qq.com>
2023-02-10 03:44:37 -0500

    :sparkles: test fiel

---
+++ internal/controller/controller_test.go
@@ -0,0 +1,45 @@
+package controller
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	dismasv1 "dismas/api/v1"
+)
+
+var _ = Describe("Job Controller", func() {
+
+	// Define objects name and spec
+	const (
+		JobName      = "test-job"
+		JobNamespace = "default"
+
+		command = "ls"
+	)
+
+	Context("When updating Job Status", func() {
+		It("Should updated Job Status about outputs", func() {
+
+			By("By creating a new job")
+			ctx := context.Background()
+			job := &dismasv1.Job{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "dismas.dismas.esphe/v1",
+					Kind:       "Job",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      JobName,
+					Namespace: JobNamespace,
+				},
+				Spec: dismasv1.JobSpec{
+					Command: command,
+				},
+			}
+			Expect(k8sClient.Create(ctx, job)).Should(Succeed())
+
+		})
+	})
+})
